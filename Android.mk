0536234dd20056eb5d294982be2d47818982170e
gutssnv <decryptor69@gmail.com>
2026-06-20 10:21:36 +0000

    X6880: initial vendor tree

---
+++ Android.mk
@@ -0,0 +1,22 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),X6880)
+
+$(call add-radio-file-sha1-checked,radio/dpm.img,ad1a20a906d1674689737f4934b4412b5cb6229a)
+$(call add-radio-file-sha1-checked,radio/gz.img,ccdfb6b7c81d3a6e2581cd6951698137d4b626c6)
+$(call add-radio-file-sha1-checked,radio/lk.img,97513c791bfb21abbb53507aea7f169adef5e05a)
+$(call add-radio-file-sha1-checked,radio/logo.img,e8b023b92674088a6dc44c817dc137fd24e82220)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,a0599ddd8865c82c33709c6dbf4171c59ff07f2e)
+$(call add-radio-file-sha1-checked,radio/md1img.img,30064bd35ead45e2739b7550675752d8aa3e76e1)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,a5c5302b9b4785c216b57ff311d76839170bc8a3)
+$(call add-radio-file-sha1-checked,radio/scp.img,b1bfb41fcdafb08c8d8aa8a5dbc02c6a777c5c67)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,77d080ba631f574c1a8d8585f32f27bf21709782)
+$(call add-radio-file-sha1-checked,radio/sspm.img,9bc46735275d39870f6ee33c38f10ad2cdf9df94)
+$(call add-radio-file-sha1-checked,radio/tee.img,1b153d80f7b4e282b83f1c9419841d0fdd10fd03)
+$(call add-radio-file-sha1-checked,radio/tkv.img,6940695be60a4eef573d4a415147961c4d41e229)
+
+endif
